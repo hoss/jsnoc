004094cd0b4c797461d2df190c07714447c4f5f9
hossgifford <hoss@flamjam.com>
2013-07-07 13:35:57 -0400

    Adding hellocanvas

---
+++ build/js/hello-canvas.js
@@ -0,0 +1,50 @@
+var Debugger = function () { };
+Debugger.log = function (message) {
+    try {
+        console.log(message);
+    } catch (exception) {
+        return;
+    }
+}
+
+function eventWindowLoaded () {
+    canvasApp();
+}
+
+function canvasSupport () {
+    return Modernizr.canvas;
+}
+
+function canvasApp () {
+    if (!canvasSupport()) {
+        return;
+    }
+
+    var theCanvas = document.getElementById("canvasOne");
+    var context = theCanvas.getContext("2d");
+
+    var canvasWidth = window.innerWidth;
+    var canvasHeight = 500;
+
+    theCanvas.width = canvasWidth;
+    theCanvas.height = canvasHeight;
+
+    Debugger.log("Drawing canvas, "+canvasWidth+"px wide, "+canvasHeight+"px high.");
+
+    var edge = 0,
+        move = 10;
+
+    function drawScreen() {
+        //background
+        context.fillStyle = "#000000";
+        context.fillRect(0, 0, canvasWidth, canvasHeight);
+        context.fillStyle = "#363636";
+        context.fillRect(0, 0, canvasWidth-(edge+=move), canvasHeight);
+        if (edge >= canvasWidth || edge < 0) {
+            move = -move;
+        }
+        window.requestAnimationFrame(drawScreen);
+    }
+
+    drawScreen();
+}
